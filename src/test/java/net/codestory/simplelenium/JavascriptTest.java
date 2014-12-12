f825734857f89f9cc41adf4ce59893025c582f72
David Gageot <david@gageot.net>
2014-12-12 18:39:43 +0100

    Format

--- src/test/java/net/codestory/simplelenium/JavascriptTest.java
+++ src/test/java/net/codestory/simplelenium/JavascriptTest.java
@@ -8,21 +8,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JavascriptTest extends AbstractTest {
   @Test
-  public void executeBasicJavaScript() {
+  public void execute_basic_javascript() {
     goTo("/");
 
     assertThat((Long) executeJavascript("return 3+1")).isEqualTo(4);
   }
 
   @Test
-  public void executeJavaScript() {
+  public void execute_javascript() {
     goTo("/");
 
     assertThat((List) executeJavascript("return window.top.document.querySelectorAll('a')")).hasSize(2);
   }
 
   @Test
-  public void execute_javaScript_with_arguments() {
+  public void execute_javascript_with_arguments() {
     goTo("/");
 
     assertThat((Long) executeJavascript("return arguments[0]+arguments[1]", 3, 1)).isEqualTo(4);
