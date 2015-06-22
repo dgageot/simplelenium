47ce0180fbf37518091addd42b9166f338a40965
David Gageot <david@gageot.net>
2015-06-22 23:05:11 +0200

    Not used

--- docker/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir old_version
-git archive --remote=.. --format=tgz master | tar zxf - -C old_version --exclude docker/*
-cd old_version
-tar zcf ../old_version.tgz *
-cd ..
-rm -Rf old_version
