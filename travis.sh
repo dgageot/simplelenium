593ac243f717977b7ce8938443dc6486b8023f4f
David Gageot <david@gageot.net>
2015-09-04 19:10:12 +0200

    wip

--- travis.sh
+++ travis.sh
@@ -24,6 +24,7 @@ CHROME)
   echo "Testing CHROME"
   curl -Lo chrome.zip https://download-chromium.appspot.com/dl/Linux_x64 && unzip chrome.zip
 	ls chrome-linux
+	pwd
   startXvfb
   mvn -Dbrowser=chrome verify
   ;;
